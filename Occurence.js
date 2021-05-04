82e6b25582e807298659ca1461dd619b557fe8e3
HONEY <honey@HONEYs-MacBook-Pro.local>
2021-05-04 15:53:00 +0530

    addedd accurence code

--- Occurence.js
+++ Occurence.js
@@ -11,25 +11,13 @@
 
 
 let findOccurence = (arr) => {
-    let result = [];
-    let count = 1;
-    let num = 0;
-    let flag = false;
-    let sortedArray = arr.sort();
-    console.log(sortedArray);
-    for(let j=0; j<sortedArray.length; j++){
-        if(sortedArray[j]!==sortedArray[j+1]){
-            count=1
-            flag=true;
-        }
-        else if (sortedArray[j]===sortedArray[j+1]){
-            num = sortedArray[j];
-            count++;
-            console.log("AAAAAAAA"+" num: "+num+ "count: "+count)
-        }
-        if(flag){
-            console.log("BBBBBBB"+" num: "+num+ "count: "+count)
-            result.push(num+"-"+count)
+    console.log(arr.sort())
+    let result = {};
+    for(var i=0; i<arr.length; i++){
+        if(!result[arr[i]]){
+            result[arr[i]] = 1
+        }else{
+            result[arr[i]]++
         }
     }
     return result;
